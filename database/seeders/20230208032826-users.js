80c6df2aa6dcedc564ab1315b4b1feb8aa98d166
Marisa145 <marisa0199914@gmail.comgit config --global user.email marisa0199914@gmail.comgit config --global user.name Marisa>
2023-03-17 18:21:37 -0300

    feat: created sequelize db:migrate

--- database/seeders/20230208032826-users.js
+++ database/seeders/20230208032826-users.js
@@ -1,42 +1,50 @@
-'use strict'
-const uuid = require('uuid')
-const { Op } = require('sequelize')
-const { hashPassword } = require('../../libs/bcrypt')
+'use strict';
+const uuid = require('uuid');
+const { Op } = require('sequelize');
+const { hashPassword } = require('../../libs/bcrypt');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    const transaction = await queryInterface.sequelize.transaction()
+    const transaction = await queryInterface.sequelize.transaction();
 
     const usersSeeds = [
       {
         id: uuid.v4(),
-        first_name: 'TEST',
-        last_name: 'TEST LN',
-        email: 'example@academlo.com',
-        username: 'example@academlo.com',
-        password: hashPassword('12345678910'),
+        first_name: 'Marisa',
+        last_name: 'Mamani',
+        email: 'marisa01999@gmail.com',
+        username: 'marisa@academlo.com',
+        password: hashPassword('627472'),
         created_at: new Date(),
         updated_at: new Date(),
       },
-    ]
+      {
+        id: uuid.v4(),
+        first_name: 'Mauricio',
+        last_name: 'Blair',
+        email: 'mauricioB@gmail.com',
+        username: 'blair@academlo.com',
+        password: hashPassword('274726'),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ];
 
     try {
-      await queryInterface.bulkInsert('users', usersSeeds, { transaction })
+      await queryInterface.bulkInsert('users', usersSeeds, { transaction });
 
-      await transaction.commit()
+      await transaction.commit();
     } catch (error) {
-      await transaction.rollback()
-      throw error
+      await transaction.rollback();
+      throw error;
     }
   },
 
   async down(queryInterface, Sequelize) {
-    const transaction = await queryInterface.sequelize.transaction()
+    const transaction = await queryInterface.sequelize.transaction();
 
-    const userNames = [
-      'example@academlo.com',
-    ]
+    const userNames = ['example@academlo.com'];
 
     try {
       await queryInterface.bulkDelete(
@@ -47,12 +55,12 @@ module.exports = {
           },
         },
         { transaction }
-      )
+      );
 
-      await transaction.commit()
+      await transaction.commit();
     } catch (error) {
-      await transaction.rollback()
-      throw error
+      await transaction.rollback();
+      throw error;
     }
   },
-}
+};
--- database/seeders/20230208033044-profiles.js
+++ database/seeders/20230208033044-profiles.js
@@ -1,54 +1,74 @@
-'use strict'
-const { Op } = require('sequelize')
-const rolesServices = require('../../services/roles.service')
-const usersServices = require('../../services/users.service')
+'use strict';
+const { Op } = require('sequelize');
+const rolesServices = require('../../services/roles.service');
+const usersServices = require('../../services/users.service');
 
-const rolesService = new rolesServices()
-const usersService = new usersServices()
+const rolesService = new rolesServices();
+const usersService = new usersServices();
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    const transaction = await queryInterface.sequelize.transaction()    
+  async up(queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
     try {
-      const adminUser = await usersService.findUserByEmailOr404('example@academlo.com')
-      const adminRole = await rolesService.findRoleByName('admin')
+      const adminUser = await usersService.findUserByEmailOr404(
+        'marisa0199914@gmail.com'
+      );
+      const adminUser2 = await usersService.findUserByEmailOr404(
+        'mauricioB@gmail.com'
+      );
+      const adminRole = await rolesService.findRoleByName('admin');
       const profiles = [
         {
           user_id: adminUser.id,
           role_id: adminRole.id,
           created_at: new Date(),
           updated_at: new Date(),
-        } 
-      ]
-      
-      await queryInterface.bulkInsert('profiles', profiles , {transaction})
-      await transaction.commit()
+        },
+        {
+          user_id: adminUser2.id,
+          role_id: adminRole.id,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ];
+
+      await queryInterface.bulkInsert('profiles', profiles, { transaction });
+      await transaction.commit();
     } catch (error) {
-      await transaction.rollback()
-      throw error
+      await transaction.rollback();
+      throw error;
     }
   },
 
-  async down (queryInterface, Sequelize) {
-    const transaction = await queryInterface.sequelize.transaction()
+  async down(queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
 
     try {
-      const adminUser = await usersService.findUserByEmailOr404('example@academlo.com')
-      const adminRole = await rolesService.findRoleByName('admin')
-      
-      await queryInterface.bulkDelete('profiles', {
-        user_id: {
-          [Op.and]: [adminUser.id]
+      const adminUser = await usersService.findUserByEmailOr404(
+        'marisa0199914@gmail.com'
+      );
+      const adminUser2 = await usersService.findUserByEmailOr404(
+        'mauricioB@gmail.com'
+      );
+      const adminRole = await rolesService.findRoleByName('admin');
+
+      await queryInterface.bulkDelete(
+        'profiles',
+        {
+          user_id: {
+            [Op.and]: [adminUser.id, adminUser2.id],
+          },
+          role_id: {
+            [Op.and]: [adminRole.id],
+          },
         },
-        role_id:{
-          [Op.and]:[adminRole.id]
-        }
-      }, { transaction })
-      await transaction.commit()
+        { transaction }
+      );
+      await transaction.commit();
     } catch (error) {
-      await transaction.rollback()
-      throw error
+      await transaction.rollback();
+      throw error;
     }
-  }
-}
+  },
+};
